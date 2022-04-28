5f97a046026e5501fe72c8987754a346e91a4033
Tiago <tiagodev@protonmail.com>
2022-04-28 17:40:59 -0300

    feat: fetching data

--- fetch/script.js
+++ fetch/script.js
@@ -1,9 +1,51 @@
-const cep = fetch("https://viacep.com.br/ws/01001000/json/ ");
+const inputCep = document.getElementById("cep");
+const btnCep = document.getElementById("btnCep");
+const resultadoCep = document.querySelector(".resultadoCep");
 
-cep
-  .then((res) => res.json())
-  .then((body) => {
-    console.log(body)
-    const text = document.querySelector(".container");
-   text.innerText = body.logradouro
-  });
+btnCep.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((res) => res.text())
+    .then((body) => {
+      resultadoCep.innerText = body;
+    });
+}
+/****************************************************************/
+// API https://blockchain.info/ticker
+const URL = "https://blockchain.info/ticker";
+const btcBrl = document.querySelector(".btc");
+
+function fetchBtc() {
+  fetch(URL)
+    .then((res) => res.json())
+    .then((btcJSON) => {
+      btcBrl.innerText = ("R$ " + btcJSON.BRL.buy).replace(".", ",");
+    });
+}
+setInterval(fetchBtc, 1000 * 300);
+
+fetchBtc();
+
+/********************************************************************/
+const chuckURL = "https://api.chucknorris.io/jokes/random";
+const buttonJoke = document.querySelector(".piadaBotao");
+const displayPiada = document.querySelector(".piada");
+
+function getJoker() {
+  fetch(chuckURL)
+    .then((res) => res.json())
+    .then((joke) => {
+      displayPiada.innerText = joke.value;
+    });
+}
+
+buttonJoke.addEventListener("click", getJoker);
+
+getJoker();
